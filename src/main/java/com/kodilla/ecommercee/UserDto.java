e006384106e227bdb34d7b67cbb7123b9022b602
Radoslaw <zerek.r93@gmail.com>
2021-05-31 23:49:12 +0200

    UserController and UserDto

---
+++ src/main/java/com/kodilla/ecommercee/UserDto.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private Long id;
+    private int key;
+    private String name;
+}
