a31122720890b9357ee4e6f78bfc839176a703aa
Maciej Kupras <maciejkupras@wp.pl>
2021-06-01 19:16:36 +0200

    ProductController_endpoint_implemantation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,45 @@
 package com.kodilla.ecommercee;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "createProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ProductDto createProduct(@RequestBody Long id) {
+        return new ProductDto(id, "Product", "product", 22);
+
+    }
+
+    @GetMapping("getProduct/{id}")
+    public ProductDto getProduct(@PathVariable Long id) {
+        return new ProductDto(id, "Product1", "product1", 22.2);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return productDto;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct/{id}")
+    public ProductDto deleteProduct(@PathVariable Long id) {
+        return new ProductDto(id, "product", "product", 22);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
 }
+
