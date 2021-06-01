ad535ce0ae6b5e9fdce1299dbfeabb9b32d0d7fb
LukaszPankiewicz <77328749+LukaszPankiewicz@users.noreply.github.com>
2021-06-01 10:55:25 +0200

    Revert "UserController and UserDto"

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,24 +1,4 @@
 package com.kodilla.ecommercee;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/v1/ecommerce")
 public class UserController {
-
-    @PostMapping(value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createUSer(@RequestBody UserDto userDto) {
-        //do nothing
-    }
-
-    @PatchMapping("/{id}/status/")
-    public UserDto blockUser(@PathVariable Long id) {
-        return new UserDto(2L, 5678, "User2");
-    }
-
-    @PatchMapping("/{id}/userKey/")
-    public UserDto createRandomUSerKey(@PathVariable Long id) {
-        return new UserDto(3L, 7890, "User3");
-    }
 }
