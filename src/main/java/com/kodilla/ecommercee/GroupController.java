ceb251d3605a9cb958bbdc2fc85265e784f79e78
Wojciech Maziarz <Wojciech255Gmail.com>
2021-06-02 14:49:23 +0200

    endpoints in GroupController

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/group")
 public class GroupController {
+
+        @GetMapping (value = "getGroups")
+        public List<String> getGroups() {
+            return new ArrayList<>();
+        }
+
+        @GetMapping (value = "getGroup")
+        public void getGroup() {
+        }
+
+        @PostMapping (value = "createGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
+        public void createGroup() {
+        }
+
+        @PutMapping (value = "updateGroup")
+        public void updateGroup() {
+        }
+
+        @DeleteMapping("deleteGroup")
+        public void deleteGroup() {
+        }
 }
