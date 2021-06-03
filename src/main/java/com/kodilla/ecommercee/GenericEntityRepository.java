2912af8ec782878a4ebf1e60f7e6e655edb171b9
Maciej Kupras <maciejkupras@wp.pl>
2021-06-03 21:31:05 +0200

    FIX_JDP210501-11

--- src/main/java/com/kodilla/ecommercee/GenericEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/GenericEntityRepository.java
@@ -1,6 +1,6 @@
 package com.kodilla.ecommercee;
 
-import git org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
