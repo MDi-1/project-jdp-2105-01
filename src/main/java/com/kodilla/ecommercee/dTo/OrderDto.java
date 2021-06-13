eba7e943b5d0e06fb80ad5c30386fe5c5b88412d
Radoslaw <zerek.r93@gmail.com>
2021-06-13 08:55:37 +0200

    Added fields userId and value in OrderDto

--- src/main/java/com/kodilla/ecommercee/dTo/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/dTo/OrderDto.java
@@ -1,6 +1,7 @@
 
 package com.kodilla.ecommercee.dTo;
 
+import com.kodilla.ecommercee.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,6 +11,8 @@ public class OrderDto {
     private Long id;
     private String name;
     private String orderStatus;
+    private User userId;
+    private double value;
 
 
 }
