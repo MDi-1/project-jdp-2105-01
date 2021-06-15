caef11931bf93b7bc3e5f8168a6a6fa3c17119c6
Marcin <calendarzyk@gmail.com>
2021-06-15 12:53:39 +0200

    JDP210501-22 in progess after rebase

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -11,6 +11,9 @@ import java.util.List;
 @RequestMapping("v1/group")
 public class GroupController {
 
+    //private final GroupService groupService;
+    //private final GroupMapper groupMapper;
+
     @GetMapping(value = "getGroups")
     public List<GroupDto> getGroups() {
         return new ArrayList<>();
