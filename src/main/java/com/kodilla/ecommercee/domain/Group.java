b1b284278388ae17d1e2cf59831a28e19f4b0286
Marcin <calendarzyk@gmail.com>
2021-06-15 17:15:00 +0200

    testGroupSuite made anew

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,35 +1,36 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-@Entity(name = "GROUP_NAME")
+@Entity(name = "GROUPS")
 public class Group {
 
     @Id
     @GeneratedValue
     @NotNull
+    @Column(name = "ID", unique = true)
     private Long id;
 
     @Column(name = "NAME")
     private String name;
 
     @OneToMany(
-            targetEntity = Product.class,
             mappedBy = "group",
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY
     )
     private List<Product> products = new ArrayList<>();
+
+    public Group(@NotNull String name) {
+        this.name = name;
+    }
 }
